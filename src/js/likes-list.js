b2d08edc1ed83f6b43d78c808cd3caecc53269b9
Zachary Smith <billyzacsmith@gmail.com>
2016-01-15 09:04:10 -0700

    Hide contact shelter button.

--- src/js/likes-list.js
+++ src/js/likes-list.js
@@ -36,7 +36,7 @@ function handleDisplayLikes() {
               '</div>' +
               '<div class="pet-info-' + i + ' mdl-card__supporting-text"></div>' +
               '<div class="mdl-card__actions mdl-card--border">' +
-                '<a id="" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="#">Contact shelter</a>' +
+                '<a id="contact-shelter" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="#">Contact shelter</a>' +
                 '<a data-id="'+pet.petfinder_id+'" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect dislikeButton" href="#">Unfriend</a>' +
 
               '</div>' +
